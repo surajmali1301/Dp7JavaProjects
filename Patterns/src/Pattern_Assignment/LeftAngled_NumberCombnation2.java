a5b3366f3caddde247f617db99d9aacff3bb113e
surajmali1301 <143394824+surajmali1301@users.noreply.github.com>
2023-09-01 00:36:58 +0530

    Pattern Assignment 31/08/2023

--- Patterns/src/Pattern_Assignment/LeftAngled_NumberCombnation2.java
+++ Patterns/src/Pattern_Assignment/LeftAngled_NumberCombnation2.java
@@ -1,23 +1,24 @@
 package Pattern_Assignment;
 
-public class LeftAngled_Combination {
+public class LeftAngled_NumberCombnation2 {
 
 	public static void main(String[] args) {
 
-		for (int i = 1; i <= 7; i++) {
+		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= i; j++) {
-				System.out.print(j + " ");
+				System.out.print(j + "");
 			}
 			System.out.println();
 		}
-		for (int i = 9; i >= 1; i-=2) {
+		for (int i = 4; i >= 1; i--) {
 //			System.out.print(i+" ");
 			for (int j = 1; j <= i; j++) {
-				System.out.print(j + " ");
+				System.out.print(j + "");
 			}
 			System.out.println();
 		}
 
 
 	}
+
 }
